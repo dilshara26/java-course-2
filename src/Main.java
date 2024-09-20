f43e724af63a274c7353a0850f3221c11f13e3c7
dilshara26 <dilsharahasanka@gmail.com>
2024-09-21 01:22:43 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,6 @@ public class Main {
         Menu menu = new Menu();
         menu.mainMenu();
 
-
-
-
 //        // Sample Doctor objects
 //        Doctor doctor1 = new Doctor(101, "John Smith", "1970-01-01", "Neurophysician", "123-456-7890");
 //        Doctor doctor2 = new Doctor(102, "Jane Doe", "1980-02-02", "Cardiologist", "987-654-3210");
